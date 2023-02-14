b2add762a1a72d2ddf827cc2442b68bddaba0721
seokwonmin-1124 <seokwonmin@naver.com>
2023-02-14 16:17:19 +0900

    dd

---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    app: {
+        token: process.env.TOKEN,
+        playing: '공부하는 척',
+        global: true,
+        guild: '1055480148587196497'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: '',
+            commands: []
+        },
+        maxVol: 100,
+        leaveOnEnd: true,
+        loopMessage: false,
+        spotifyBridge: true,
+        defaultvolume: 75,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
